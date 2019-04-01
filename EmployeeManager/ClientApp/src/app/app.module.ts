60e014d0a1bc146b06b68a5978ca63638275b1be
Paulo Coelho <paulo.coelho1989@gmail.com>
2019-04-01 12:40:08 +0100

    Adds new Employee model Adds new nav component Adds new home component Adds new sidebar component Adds global css styles Adds some page layouts for home, nav and sidebar

--- EmployeeManager/ClientApp/src/app/app.module.ts
+++ EmployeeManager/ClientApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ EmployeeManager/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Employee> GetAll()
+        {
+            var list = new List<Employee>();
+
+            list.Add(new Employee() { Name = "Lilly" });
+            list.Add(new Employee() { Name = "Lucy" });
+
+            return list;
+        }
+
+        [HttpGet("{name}")]
+        public Employee Get(string name)
+        {
+            return new Employee() { Name = name };
+        }
+
+        [HttpPost]
+        public Employee Insert([FromBody]Employee cat)
+        {
+            // write the new cat to database
+            return cat;
+        }
+
+        [HttpPut("{name}")]
+        public Employee Update(string name, [FromBody]Employee cat)
+        {
+            cat.Name = name;
+            // write the updated cat to database
+            return cat;
+        }
+
+        [HttpDelete("{name}")]
+        public void Delete(string name)
+        {
+            // delete the cat from the database
+
+        }
+    }
+}
